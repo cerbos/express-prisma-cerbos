5417e957b72266529719b6316efe0a0712db83d2
Alex Olivier <alex@alexolivier.me>
2021-08-30 13:12:13 +0100

    docs

--- README.md
+++ README.md
@@ -35,20 +35,34 @@ npx prisma db seed --preview-feature
 npm run dev
 ```
 
+## Seed Users
+
+The Prisma seed command will create the following users in the database. Authentication is done via Basic authentication using the following credentials. The Role and Department is loaded from the database after successful authentication.
+
+| ID  | Username | Password     | Role  | Department |
+| --- | -------- | ------------ | ----- | ---------- |
+| 1   | alice    | supersecret  | Admin | IT         |
+| 2   | john     | password1234 | User  | Sales      |
+| 3   | sarah    | asdfghjkl    | User  | Sales      |
+| 4   | geri     | pwd123       | User  | Marketing  |
+
 ## Policies
 
 This example has a simple CRUD policy in place for a resource kind of `contact` - like a CRM system would have. The policy file can be found in the `cerbos/policies` folder [here](https://github.com/cerbos/express-prisma-cerbos/blob/main/cerbos/policies/contact.yaml).
 
-Should you wish to experiment with this policy, you can <a href="https://play.cerbos.dev/p/sZC611cf06deexP0q8CTcVufTVau1SA3" target="_blank">try it in the Cerbos Playground</a>.
+Should you wish to experiment with this policy, you can <a href="https://play.cerbos.dev/p/ygW612cc9c9xXOsOZjI40ovY2LZvXf43" target="_blank">try it in the Cerbos Playground</a>.
 
-<a href="https://play.cerbos.dev/p/sZC611cf06deexP0q8CTcVufTVau1SA3" target="_blank"><img src="docs/launch.jpg" height="48" /></a>
+<a href="https://play.cerbos.dev/p/ygW612cc9c9xXOsOZjI40ovY2LZvXf43" target="_blank"><img src="docs/launch.jpg" height="48" /></a>
 
-The policy expects one of two roles to be set on the principal - `admin` and `user`. These roles are authorized as follows:
+The policy expects one of two roles to be set on the principal - `admin` and `user` and an attribute which defines their department as either `IT`, `Sales` or `Marketing`.
 
-| Action | User | Admin |
-| ------ | ---- | ----- |
-| list   | Y    | Y     |
-| read   | Y    | Y     |
-| create | N    | Y     |
-| update | N    | Y     |
-| delete | N    | Y     |
+These roles are authorized as follows:
+
+| Action   | Role: User                                  | Role: Admin |
+| -------- | ------------------------------------------- | ----------- |
+| `read`   | Only if department is `Sales`               | Y           |
+| `create` | Only if department is `Sales`               | Y           |
+| `update` | Only if they own the contact being accessed | Y           |
+| `delete` | Only if they own the contact being accessed | Y           |
+
+## Example Requests
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ const cerbos = new Cerbos({
 });
 
 app.use(express.json());
+
+// Swap out for your authentication provider of choice
 app.use(
   basicAuth({
     users: {
@@ -50,7 +52,7 @@ app.get("/contacts/:id", async (req, res) => {
   // check user is authorized
   const allowed = await cerbos.check({
     principal: {
-      id: user.id.toString(),
+      id: `${user.id}`,
       roles: [user.role],
       attr: {
         department: user.department,
@@ -70,6 +72,131 @@ app.get("/contacts/:id", async (req, res) => {
   // authorized for read action
   if (allowed.isAuthorized(`${contact.id}`, "read")) {
     return res.json(contact);
+  } else {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+});
+
+app.post("/contacts/new", async (req, res) => {
+  // Get the user
+  const user = await getUser(req);
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  // check user is authorized
+  const allowed = await cerbos.check({
+    principal: {
+      id: `${user.id}`,
+      roles: [user.role],
+      attr: {
+        department: user.department,
+      },
+    },
+    resource: {
+      kind: "contact",
+      instances: {
+        new: {},
+      },
+    },
+    actions: ["create"],
+  });
+
+  // authorized for create action
+  if (allowed.isAuthorized("new", "create")) {
+    return res.json({ result: "Created contact" });
+  } else {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+});
+
+app.patch("/contacts/:id", async (req, res) => {
+  // load the contact
+  const contact = await prisma.contact.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    include: {
+      company: true,
+    },
+  });
+  if (!contact) return res.status(404).json({ error: "Contact not found" });
+
+  // Get the user
+  const user = await getUser(req);
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  const allowed = await cerbos.check({
+    principal: {
+      id: `${user.id}`,
+      roles: [user.role],
+      attr: {
+        department: user.department,
+      },
+    },
+    resource: {
+      kind: "contact",
+      instances: {
+        [contact.id]: {
+          attr: contact,
+        },
+      },
+    },
+    actions: ["update"],
+  });
+
+  if (allowed.isAuthorized(req.params.id, "update")) {
+    return res.json({
+      result: `Updated contact ${req.params.id}`,
+    });
+  } else {
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+});
+
+// DELETE
+app.delete("/contacts/:id", async (req, res) => {
+  // load the contact
+  const contact = await prisma.contact.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    include: {
+      company: true,
+    },
+  });
+  if (!contact) return res.status(404).json({ error: "Contact not found" });
+
+  // Get the user
+  const user = await getUser(req);
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  const allowed = await cerbos.check({
+    principal: {
+      id: `${user.id}`,
+      roles: [user.role],
+      attr: {
+        department: user.department,
+      },
+    },
+    resource: {
+      kind: "contact",
+      instances: {
+        [contact.id]: {
+          attr: contact,
+        },
+      },
+    },
+    actions: ["delete"],
+  });
+
+  if (allowed.isAuthorized(req.params.id, "delete")) {
+    prisma.contact.delete({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    return res.json({
+      result: `Contact ${req.params.id} deleted`,
+    });
   } else {
     return res.status(403).json({ error: "Unauthorized" });
   }
