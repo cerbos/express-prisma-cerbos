699cc26fbe44415a8e4c8c72f0cda964db427f6f
Dennis Buduev <dbuduev@users.noreply.github.com>
2021-11-26 17:49:18 +1300

    WIP

--- cerbos/start.sh
+++ cerbos/start.sh
@@ -1,5 +1,5 @@
 docker run -i -t -p 3592:3592 \
   -v $(pwd)/config:/config \
   -v $(pwd)/policies:/policies \
-  ghcr.io/cerbos/cerbos:0.10.0-prerelease-arm64 \
+  dbuduev/cerbos:0.10.0-prerelease \
   server --config=/config/conf.yaml
